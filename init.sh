1822d01e19bad027262c3bed17196db171fc111b
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2021-03-26 14:19:34 +0000

    :truck: Build once based on config.toml

--- init.sh
+++ init.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
-while getopts h:i:l: option
-do
-    case "${option}"
-        in
-        h) HOST=${OPTARG};;
-        i) ID=${OPTARG};;
-        l) LAYER=${OPTARG};;
-    esac
-done
 
 echo "Checking for pem files"
 if [ ! -f /root/.nym/mixnode/mixer/data/private_sphinx.pem ] || [ ! -f /root/.nym/mixnode/mixer/data/public_sphinx.pem ]; then
     echo "Missing pem files, running init"
-    nym-mixnode init --host ${HOST} --id ${ID} --layer ${LAYER}
+    nym-mixnode init --id mixer
 fi
 if [ -f /root/.nym/mixnode/mixer/data/private_sphinx.pem ] && [ -f /root/.nym/mixnode/mixer/data/public_sphinx.pem ]; then
     echo "Pem files found"
