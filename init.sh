fb9e97afab2ce64c0f38f958ef6febbd9716ffda
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2021-03-26 14:58:41 +0000

    :bug: --host is required

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 echo "Checking for pem files"
 if [ ! -f /root/.nym/mixnode/mixer/data/private_sphinx.pem ] || [ ! -f /root/.nym/mixnode/mixer/data/public_sphinx.pem ]; then
     echo "Missing pem files, running init"
-    nym-mixnode init --id mixer
+    nym-mixnode init --id mixer --host=0.0.0.0
 fi
 if [ -f /root/.nym/mixnode/mixer/data/private_sphinx.pem ] && [ -f /root/.nym/mixnode/mixer/data/public_sphinx.pem ]; then
     echo "Pem files found"
